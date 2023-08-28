ebabbabc6522e0e26073caa0ecb68a1b805cb7f5
perosky168 <ogunbanjopetet10@gmail.com>
2023-08-28 06:09:49 +0100

    created setup

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import express, { Express } from 'express';
+import { ChatAppServer } from './setupServer';
+import databaseConnention from './setupDatabase';
+import { config } from './config';
+
+class Application {
+  public start(): void{
+    this.loadConfig();
+    databaseConnention();
+    const app: Express = express();
+    const server: ChatAppServer = new ChatAppServer(app);
+    server.start();
+  }
+
+  private loadConfig(): void {
+    config.validateConfig();
+  }
+}
+
+const application: Application = new Application();
+application.start();
