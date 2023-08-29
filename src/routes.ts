e0983f45550491053f3261edc1cc50e63c75fc24
perosky168 <ogunbanjopetet10@gmail.com>
2023-08-29 02:17:54 +0100

    joi validation and creating user works

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,11 @@
-import { Application } from "express"
+import { Application } from 'express';
+import { authRoutes } from './features/auth/routes/authRoute';
+
+const BASE_PATH = '/api/v1';
 
 export default (app: Application) => {
   const routes = () => {
-    
+    app.use(BASE_PATH, authRoutes.routes());
   };
   routes();
-}
+};
