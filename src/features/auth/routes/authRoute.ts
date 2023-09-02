65a2c53f7a8f4886050fcd94ee6c16b705117c6f
perosky168 <ogunbanjopetet10@gmail.com>
2023-09-02 01:36:17 +0100

    added signin and signout funtionalities

--- src/features/auth/routes/authRoute.ts
+++ src/features/auth/routes/authRoute.ts
@@ -1,5 +1,7 @@
 import express, { Router } from 'express';
 import { SignUp } from '../controllers/signup';
+import { signIn } from '../controllers/signin';
+import { SignOut } from '../controllers/signout';
 
 class AuthRoutes {
   private router: Router;
@@ -9,6 +11,13 @@ class AuthRoutes {
 
   public routes(): Router {
     this.router.post('/signup', SignUp.prototype.create);
+    this.router.post('/signin', signIn.prototype.read);
+
+    return this.router;
+  }
+
+  public signoutRoutes(): Router {
+    this.router.get('/signout', SignOut.prototype.update);
 
     return this.router;
   }
